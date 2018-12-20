fae0d68ac2c259ee27e4628ac5722ff1ec4725a3
MAHOfficial <admin@mahofficial.win>
2018-12-20 18:55:56 +0500

    Added 4 Social Media
    - Facebook
    - Twitter
    - Google Plus
    - Linkedin

--- config.php
+++ config.php
@@ -8,6 +8,14 @@ $mofh = array(
 'protocol' => 'https://', // it is https or http
 
  'mail' => 'some@mofh.ooo', // email here
+     
+ 'facebook' => 'https://www.facebook.com/Arcenas090', // your facebook link
+     
+ 'twitter' => 'https://twitter.com/Arcenas090', // your twitter link
+     
+ 'google-plus' => 'https://plus.google.com/+Arcenas090', // your google plus link
+     
+ 'linkedin' => 'https://www.linkedin.com/in/Arcenas090', // your linkedin link
 
 'domain' => strtolower(preg_replace('/^www\./' , '' , $_SERVER['HTTP_HOST'])), // Automatic Domain Recognition
 
